5db58634e34d5a9c4702c058096b094d7ec809fb
SyndicateCID <charrodterrell@yahoo.com>
2025-06-16 15:43:58 -0500

    js working

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ const q = 'comdey'
 const apikey = '4dq04fkDkwiXsn1xH9GQZCLgQarb2sux&q'
 const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`
 
-const p = fetch(JSON)
+const promise1 = fetch(path)
+const promise2 = promise.then(function(response) {
+    return response.json()
+})
 
+promise2.then(function(json) {
+    console.log(json)
+})
