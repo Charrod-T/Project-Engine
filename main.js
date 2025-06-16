45bc42a2ba517f92a38c39e52f69e5cc7b99b2a0
SyndicateCID <charrodterrell@yahoo.com>
2025-06-16 16:16:29 -0500

    add json data/url

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const q = 'comdey'
 const apikey = '4dq04fkDkwiXsn1xH9GQZCLgQarb2sux&q'
 const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`
 
-const promise1 = fetch(path)
-const promise2 = promise.then(function(response) {
+fetch(path)
+.then( function (response) {
     return response.json()
 })
-
-promise2.then(function(json) {
-    console.log(json)
+.then( function (json) {
+    console.log(json.data[0].images.fixed_width.url)
+        json.data.forEach(function(object) {
+        console.log(object.images.fixed_width.url)
+    })
+})
+.catch(function(error) {
+console.log(error.message)
 })
