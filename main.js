44fecba32b6a5e75ee7884bd19ce7482a57594f5
SyndicateCID <charrodterrell@yahoo.com>
2025-06-16 22:57:44 -0500

    js file working

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const searchForm = document.getElementById ('search-form')
+const searchBox = document.getElementById ('search-box')
+const resultsElement = document.getElementById('results')
 
 searchForm.addEventListener('submit', function(event) {
-event.preventDefault()
+    event.preventDefault()
+    const q = searchBox.value
+    search(q)
 })
 
 function search(q) {
@@ -9,21 +13,20 @@ const apikey = '4dq04fkDkwiXsn1xH9GQZCLgQarb2sux';
 const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`;
 
 
-function search(q) 
-fetch(path).then ( function (response) {
-return response.json()
+fetch(path).then (function (response) {
+    return response.json()
+})
 
-}).then(function (json) {
-console.log(json.data[0].images.fixed_width.url
-const resultsElement = document.getElementById('results')
+.then(function (json) {
+console.log(json.data[0].images.fixed_width.url)
 let resultsHTML = ''
 
 json.data.forEach(function(object) {
 console.log(object)
 
-const url = obejct.images.fixed_width.url
-const width = obejct.images.fixed_width
-const height = obejct.images.fixed_width.height
+const url = object.images.fixed_width.url
+const width = object.images.fixed_width.width
+const height = object.images.fixed_width.height
 const title = object.title
 
 resultsHTML += `<img src = "${url}" 
@@ -32,7 +35,7 @@ resultsHTML += `<img src = "${url}"
                  alt = "${title}">`
 })
 
-resultsElement.innerHTML = resultHTML
+resultsElement.innerHTML = resultsHTML
 
 }).catch(function (error) {
 console.log (error.message)
